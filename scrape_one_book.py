fd371d75c13c030075ee35e4611664cd22072c22
Yusan Lin <yusan@psu.edu>
2014-10-23 10:56:09 -0400

    update scraping one book

--- scrape_one_book.py
+++ scrape_one_book.py
@@ -18,8 +18,18 @@ response = urllib2.urlopen(u)
 html = response.read()
 soup = BeautifulSoup(html)
 
+# these are the features (attributes)
 title = soup.find("h1", class_="milo").get_text().strip()
 author = soup.find("a", class_="subtle").get_text().strip()
 price = soup.find("div", class_="price hilight").get_text().strip()
-rating_text = soup.find("div", class_="price hilight").get_text()
+#subject
+
+product_detail_box = list(soup.find("div", class_="product-details box").descendants)
+publisher = product_detail_box[product_detail_box.index("Publisher: ") + 1]
+date = product_detail_box[product_detail_box.index("Publication date: ") + 1]
+pages = product_detail_box[product_detail_box.index("Pages: ") + 1]
+overview = soup.find_all("div", class_="simple-html")[1].get_text()
+
+# this is our final classification label (ground truth)
+rating_text = soup.find("span", class_="stars-large r3h")['title']
 rating = rating_text[rating_text.find("of")+3 : rating_text.find("out")-1]
